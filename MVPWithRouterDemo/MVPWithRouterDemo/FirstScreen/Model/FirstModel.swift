ef02e2baca3f1ee0a3764d9f5abededa49062b5c
Sergey <>
2023-07-16 13:27:09 +0400

    create first model class

--- MVPWithRouterDemo/MVPWithRouterDemo/FirstScreen/Model/FirstModel.swift
+++ MVPWithRouterDemo/MVPWithRouterDemo/FirstScreen/Model/FirstModel.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+protocol FirstModelProtocol {
+    func getData() -> String
+    func setData(data: String)
+}
+
 final class FirstModel {
+    private var someData: String?
+}
+
+extension FirstModel: FirstModelProtocol {
+    func getData() -> String {
+        guard let someData = self.someData else { return "" }
+        return someData
+    }
     
+    func setData(data: String) {
+        self.someData = data
+    }
 }
